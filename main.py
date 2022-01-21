379edc29421ff15722c2f8d5ae0396a1a21985f0
Burdinskaia <78086736+Burdinskaia@users.noreply.github.com>
2022-01-21 07:23:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+text = input("Ievadit tekstu: ")
+def replaceO (text):
+  if text.count("o")>0 or text.count("o")>0:
+    text = text.replace("o","%")
+    text = text.replace("o","%")
+    print(text)
+  else:
+    text = "Nav burtu O vai o." 
+    print(text)
+  return text
+replaceO (text)
